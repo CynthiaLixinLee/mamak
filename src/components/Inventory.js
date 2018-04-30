0d51f3bdb366eb54da755fd51d30c7fcd5058b41
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-04-29 23:49:24 -0400

    Set up authentication and added mobile responsiveness

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 import AddDish from "./AddDish";
 import EditDish from "./EditDish";
+import Login from "./Login";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -9,8 +12,65 @@ class Inventory extends React.Component {
     updateDish: PropTypes.func,
     deleteDish: PropTypes.func
   };
+  // This state is local to this component
+  state = {
+    uid: null,
+    owner: null
+  };
+
+  // Everytime we load page, firebase will check if we're logged in
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+      }
+    });
+  }
+
+  authHandler = async authData => {
+    const storeOwner = await base.fetch("owner", { context: this });
+    // Set the state of the inventory component to reflect current user
+    this.setState({
+      uid: authData.user.uid,
+      owner: storeOwner
+    });
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
+  logout = async () => {
+    console.log("Logging out...");
+    await firebase.auth().signOut();
+    this.setState({ uid: null }); // Reset state
+  };
 
   render() {
+    const logout = <button onClick={this.logout}>Log Out!</button>;
+
+    // Check if user is logged in
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+    // Check if they are not the owner
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <h3 className="slogan">
+            We use the freshest produce and traditional recipies to create good
+            food
+          </h3>
+          <p>Sorry, you are not the owner of this store!</p>
+          {logout}
+        </div>
+      );
+    }
+    // They must be the owner - render the inventory
     return (
       <div className="inventory">
         <h2>Inventory</h2>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Login = props => (
-  <nav className="login">
-    <h2>Inventory Login</h2>
-    <p>Sign in to manage your store's inventory.</p>
-    <button
-      className="github"
-      // It is not "this.props" because Login is a stateless functional component
-      onClick={() => props.authenticate("Github")}
-    >
-      Log In With Github
-    </button>
-    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
-      Log In With Facebook
-    </button>
+  <nav>
+    <h3 className="slogan">
+      We use the freshest produce and traditional recipies to create good food
+    </h3>
+    <div className="login">
+      <h2>Inventory Login</h2>
+      <p>Sign in to manage your store's inventory.</p>
+      <button
+        className="github"
+        // It is not "this.props" because Login is a stateless functional component
+        onClick={() => props.authenticate("Github")}
+      >
+        Log In With Github
+      </button>
+      <button
+        className="facebook"
+        onClick={() => props.authenticate("Facebook")}
+      >
+        Log In With Facebook
+      </button>
+    </div>
   </nav>
 );
 
